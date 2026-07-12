eeb203ff1c04268450fba02945e817c8bc56c373
Vedant Modi <vedantmodi@gmail.com>
2026-07-12 17:10:16 +0000

    framelog 0.12.1

--- Casks/framelog.rb
+++ Casks/framelog.rb
@@ -1,6 +1,6 @@
 cask "framelog" do
-  version "0.12.0"
-  sha256 "564d5e42a0481932e27b3a0c0be768cb11164ad512300c1e3b23bbba90a89be4"
+  version "0.12.1"
+  sha256 "ba2de9c0e1162ed362cb8ba07a66f9c8ff2e8508295357e2264778ff9116b3b9"
 
   url "https://github.com/thevedantmodi/framelog/releases/download/v#{version}/Framelog-#{version}.dmg"
   name "Framelog"
